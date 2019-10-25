25fcf6126efa49f77c8ecf99e1876c717324ac6b
Christopher Kelly <2313175k@student.gla.ac.uk>
2019-10-25 22:18:27 +0100

    Christopher Kelly - Could not push using local repo so took the file and added it here. Basic scraper that returns title and url

---
+++ scraper.py
@@ -0,0 +1,27 @@
+import youtube_dl
+import json
+
+
+# only works for single videos at the moment and not embedded
+# method for getting the url and title from the page the user is on
+# requires url pulled from the user
+
+def Extractor(url):
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+    video = {'URL': '', 'Title': ''}
+    with ydl:
+        result = ydl.extract_info(
+            url,
+            download=False  # We just want to extract the info
+        )
+
+    video['URL'] = result["webpage_url"]
+    video['Title'] = result["title"]
+
+    return video
+
+# below will be how the data is changed to JSON format and how to invoke
+# the function
+
+# Dict = Extractor("https://www.facebook.com/oldrow/videos/801877526899085/")
+# JSON = json.dumps(Dict)
