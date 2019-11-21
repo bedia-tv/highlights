e7e6a47eb2de358d8ca925bd044171ee5393929e
Arut Jinadit <arut.jinadit@gmail.com>
2019-11-21 23:22:12 +0000

    [Added]: firebase and a share page.

--- frontend/apps/pwa-extension/src/app/app.tsx
+++ frontend/apps/pwa-extension/src/app/app.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import { Container } from './app.style';
 
 const App = () => {
   return (
-    <Container>
-      <h1>Video Submission</h1>
-    </Container>
-  )
-}
+    <Switch>
+      <Route path="/" exact>
+        <Container>
+          <h1>Video Submission</h1>
+          <Link to="/share">
+            <h3>
+              Share ?
+            </h3>
+          </Link>
+        </Container>
+      </Route>
+      <Route path="/share" exact>
+        <Container>
+          <h1>Share</h1>
+        </Container>
+      </Route>
+    </Switch>
+  );
+};
 
 export default App;
