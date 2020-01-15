f3be491e4daa7f250b53d669fadf9491a75830d6
Arut Jinadit <arut.jinadit@gmail.com>
2020-01-15 12:28:17 +0000

    [Added]: CORS issue solver

--- frontend/libs/components/src/lib/graphql/client.tsx
+++ frontend/libs/components/src/lib/graphql/client.tsx
@@ -4,11 +4,33 @@ import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import {ApolloProvider} from "@apollo/react-hooks"
 import React from 'react';
 
+let host = process.env.HOST  || 'http://localhost'
+let port = process.env.PORT || '8000';
+
+const url = `${host}:${port}/graphql`;
+
+function getCookie(name)  {
+  let cookieValue = null;
+  if(document.cookie  && document.cookie !== '') {
+    let cookies = document.cookie.split(":");
+    for (let i = 0;i < cookies.length; i++) {
+      let cookie = cookies[i].trim()
+      if (cookie.substring(0, name.length + 1) === (name + "=")) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:8000/graphql',
   credentials: 'include',
+  headers: {
+    'X-CSRFToken': getCookie('csrftoken')
+  }
 });
-
 
 export const client = new ApolloClient({
   link: httpLink,
