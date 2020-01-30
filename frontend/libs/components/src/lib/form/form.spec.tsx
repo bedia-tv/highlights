16db4e6f75b9914e1922aab2caa2c0e40446074d
Arut Jinadit <arut.jinadit@gmail.com>
2020-01-30 16:01:55 +0000

    [Added]: Apollo testing library and DOM testing library.

--- frontend/libs/components/src/lib/form/form.spec.tsx
+++ frontend/libs/components/src/lib/form/form.spec.tsx
@@ -1,11 +1,31 @@
+import '@testing-library/jest-dom/extend-expect'
 import React from 'react';
+import {MockedProvider} from '@apollo/react-testing';
 import { render } from '@testing-library/react';
+import {Form} from './form';
+import {SUBMIT_VIDEO_MUTATION} from '../graphql/video'
 
-import Form from './form';
+const mocks = [
+  {
+    request: {
+      query: SUBMIT_VIDEO_MUTATION
+    },
+    result: {
+      data: {
+        success: true,
+      }
+    }
+  }
+];
 
-describe(' Form', () => {
+describe('Form', () => {
+
   it('should render successfully', () => {
-    const { baseElement } = render(<Form />);
+    const { baseElement } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Form url="https://youtube.com"/>  
+      </MockedProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
