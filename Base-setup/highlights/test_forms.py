faa58350761ffd3fee460c4ec8564536aba74ec8
YingTing Long <2505861L@student.gla.ac.uk>
2020-02-19 00:25:28 +0000

    [Modified] complete unit testing form

--- Base-setup/highlights/test_forms.py
+++ Base-setup/highlights/test_forms.py
@@ -4,7 +4,7 @@ from graphene import Schema
 from highlights.schema import Query, Mutation
 from django.test.utils import override_settings
 
-class VideosQueryTest(TestCase):
+class VideosTest(TestCase):
     @override_settings(DEBUG=True)
     def setUp(self):
         querySchema = Schema(query=Query)
@@ -79,24 +79,116 @@ class VideosQueryTest(TestCase):
         else:
             raise Exception("Mutation of createVideo failed!")
 
-  '''
-  
-  mutation y{
-            createHighlight(input:{
-                highlightName: "highlightName"
+class HighlightsTest(TestCase):
+    @override_settings(DEBUG=True)
+    def setUp(self):
+        querySchema = Schema(query=Query)
+        self.queryClient = Client(schema = querySchema)
+        mutationSchema = Schema(mutation = Mutation)
+        self.mutationClient = Client(schema = mutationSchema)
+
+    def test_create_highlight(self):
+        mutation = self.mutationClient.execute('''
+        mutation {
+            createVideo(input:{
                 url: "www.example.com"
-              	startTime: "12.30"
-              	endTime: "14.53"
-              	
+                title: "title"
             }){
                 ok
-                highlight{
-                    highlightName
-                  	startTime
-                  	endTime
-                  	comments
-                }
             }  
-        }'''
+        }
+        ''')
+        if mutation["data"]["createVideo"]["ok"]:
+            executed = self.mutationClient.execute('''
+            mutation {
+                createHighlight(input:{
+                    highlightName: "highlightName"
+                    videoTitle: "video_title"
+                    comments: "comments"
+                    url: "www.example.com"
+              	    startTime: "12.30"
+              	    endTime: "14.53"             	
+                }){
+                    ok
+                    highlight{
+                        highlightName
+                  	    startTime
+                  	    endTime
+                  	    comments
+                    }
+                }  
+            }
+            ''')
+            assert executed == {
+                "data": {
+                    "createHighlight": {
+                        "ok": True,
+                        "highlight": {
+                            "highlightName": "highlightName",
+                            "startTime": 12.3,
+                            "endTime": 14.53,
+                            "comments": "comments"
+                            }
+                    }
+                }
+            }
+        else:
+            raise Exception("Mutation of createVideo failed!")
+
+    def test_get_highlight(self):
+        mutation = self.mutationClient.execute('''
+        mutation {
+            createVideo(input:{
+                url: "www.example.com"
+                title: "title"
+            }){
+                ok
+            }  
+        }
+        ''')
+        if mutation["data"]["createVideo"]["ok"]:
+            executed = self.mutationClient.execute('''
+            mutation {
+                createHighlight(input:{
+                    highlightName: "highlightName"
+                    videoTitle: "video_title"
+                    comments: "comments"
+                    url: "www.example.com"
+              	    startTime: "12.30"
+              	    endTime: "14.53"             	
+                }){
+                    ok
+                    highlight{
+                        highlightName
+                  	    startTime
+                  	    endTime
+                  	    comments
+                    }
+                }  
+            }
+            ''')
+            print(executed)
+            if executed["data"]["createHighlight"]["ok"]:
+                assert executed == {
+                    "data": {
+                        "createHighlight": {
+                        "ok": True,
+                        "highlight": {
+                            "highlightName": "highlightName",
+                            "startTime": 12.3,
+                            "endTime": 14.53,
+                            "comments": "comments"
+                            }
+                        }
+                    }
+                }
+            
+            else:
+                raise Exception("Mutation of createHighlight failed!")
+        else:
+            raise Exception("Mutation of createVideo failed!")
+
+
+
 
 
